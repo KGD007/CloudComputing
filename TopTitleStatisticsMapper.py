78b04cce7e35886602879879736f013bdc6b3ea8
kgd007 <gaurav.dubey61@gmail.com>
2021-02-28 21:58:41 -0500

    update

--- TopTitleStatisticsMapper.py
+++ TopTitleStatisticsMapper.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 import sys
 
-
+words = {}
 for line in sys.stdin:
-    # TODO
+    words[line.split()[0]] = line.split()[1]
     
     # print('%s\t%s' % (  ,  )) pass this output to reducer
+print(words)
--- TopTitleStatisticsReducer.py
+++ TopTitleStatisticsReducer.py
@@ -3,10 +3,21 @@ import sys
 import math
 
 
-#TODO
-
+words = {}
+allvalues = []
 for line in sys.stdin:
-    # TODO
+    words = eval(line)
+    for key, value in words.items():
+        allvalues.append(int(value))
 
 #TODO
+mean =  math.floor(sum(allvalues) / 10)
+variance = math.floor(sum([ (mean - i)**2 for i in allvalues]))
+print('Mean'+ '\t' + str(mean))
+print('Sum'+ '\t' + str(sum(allvalues)))
+print('Min'+ '\t' + str(min(allvalues)))
+print('Max'+ '\t' + str(max(allvalues)))
+print('Var'+ '\t' + str(variance))
+
+
 # print('%s\t%s' % (  ,  )) print as final output
