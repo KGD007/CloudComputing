a8dfbbff9e63b5cae8c13ec2fffaa9c59910ec14
kgd007 <gaurav.dubey61@gmail.com>
2021-02-28 20:59:16 -0500

    update

--- TopTitlesReducer.py
+++ TopTitlesReducer.py
@@ -8,7 +8,7 @@ for line in sys.stdin:
     topTitle = eval(line)
 
 topTitle = sorted(topTitle.items(), key= lambda x : -x[1])
-for i, j in topTitle[:10][::-1]:
+for i, j in topTitle[:10]:
     print(i+ "  " + str(j))
 
     # print('%s\t%s' % (  ,  )) print as final output
