40a784fa4e1e1f4c9a8dc6bad413892816f4c198
kgd007 <gaurav.dubey61@gmail.com>
2021-03-01 22:30:30 -0500

    update

--- LinkCountMapper.py
+++ LinkCountMapper.py
@@ -4,5 +4,5 @@ output = ""
 for line in sys.stdin:
   key , values = line.split(':')[0], line.split(':')[1]
   for value in values.strip().split(' '):
-    output += key + ':'
+    output += value + ':'
 print(output)
