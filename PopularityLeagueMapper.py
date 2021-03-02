5cbcf9860cc840435c7d0c757ea6266740a2de86
kgd007 <gaurav.dubey61@gmail.com>
2021-03-01 23:34:20 -0500

    update

--- PopularityLeagueMapper.py
+++ PopularityLeagueMapper.py
@@ -3,18 +3,27 @@ import sys
 
 
 leaguePath = sys.argv[1]
-#TODO
 
-
+links = {}
+test = []
 with open(leaguePath) as f:
-	#TODO
-
-
-
+       words = f.readlines()
+       for word in words:
+              test.append(word.strip().lower())
 
 
 for line in sys.stdin:
+       dictionary = eval(line)
+       for key , value in dictionary.items():
+              if key not in links:
+                     links[key] = int(value)
+              else:
+                     links[key] += int(value)
 
-       #TODO
-
-       # print('%s\t%s' % (  ,  )) pass this output to reducer
+output = {}
+for key in test:
+      if key in links:
+              output[key] = links[key]
+       else:
+              output[key] = 0
+print(output)
--- PopularityLeagueReducer.py
+++ PopularityLeagueReducer.py
@@ -1,11 +1,25 @@
 #!/usr/bin/env python3
 import sys
-#TODO
+links = {}
 
 # input comes from STDIN
 for line in sys.stdin:
-    # TODO
+    links.update(eval(line))
 
+for key, value in links.items():
+    links[key] = int(value)
 
-#TODO
+links = sorted(links.items(), key = lambda x : x[1])
+output = []
+output.append((links[0], 0))
+
+for i in range(1, len(links)):
+    if links[i][1] == links[i-1][1]:
+        output.append((links[i], output[i-1][1]))
+    else:
+        output.append((links[i], i))
+
+output = sorted(output, lambda x: int(x[0]))
+for i, j in output:
+    print(str(i) + '\t' + str(j))
 # print('%s\t%s' % (  ,  )) print as final output
