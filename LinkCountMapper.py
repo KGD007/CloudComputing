c8f06d6f62fe2e013e92a9d9aa0d0c25a9073bb4
kgd007 <gaurav.dubey61@gmail.com>
2021-03-01 21:53:31 -0500

    update

--- LinkCountMapper.py
+++ LinkCountMapper.py
@@ -3,6 +3,6 @@ import sys
 output = ""
 for line in sys.stdin:
   key , values = line.split(':')[0], line.split(':')[1]
-  for value in values:
+  for value in values.strip().split('\t'):
     output += key + ':'
 print(output)
